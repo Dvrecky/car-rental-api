ad4984e62fa3eeb3db52030f5ab91d91665a0301
Dvrecky <hubertdurka43@gmail.com>
2025-01-07 17:19:02 +0100

    Creating CarService and CarServiceImpl. Adding method that returns CarDTOs list

---
+++ src/main/java/pl/myproject/car_rental_api/service/CarService.java
@@ -0,0 +1,10 @@
+package pl.myproject.car_rental_api.service;
+
+import pl.myproject.car_rental_api.dto.CarDTO;
+
+import java.util.List;
+
+public interface CarService {
+
+    List<CarDTO> getAllCarsWithDetails();
+}
---
+++ src/main/java/pl/myproject/car_rental_api/service/impl/CarServiceImpl.java
@@ -0,0 +1,34 @@
+package pl.myproject.car_rental_api.service.impl;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.myproject.car_rental_api.dto.CarDTO;
+import pl.myproject.car_rental_api.entity.Car;
+import pl.myproject.car_rental_api.repository.CarRepository;
+import pl.myproject.car_rental_api.service.CarService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private final CarRepository carRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public CarServiceImpl(CarRepository carRepository, ModelMapper modelMapper) {
+        this.carRepository = carRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public List<CarDTO> getAllCarsWithDetails() {
+
+        return carRepository.getAllCarsWithDetails()
+                .stream()
+                .map(entity -> modelMapper.map(entity, CarDTO.class))
+                .toList();
+    }
+}
