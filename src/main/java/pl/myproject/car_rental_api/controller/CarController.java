92bf1b097adc3ff4c9810bc47d08089753b7a98a
Dvrecky <hubertdurka43@gmail.com>
2025-01-09 11:22:21 +0100

    Creating CarController. Creating GET method for retrieving all cars with details

---
+++ src/main/java/pl/myproject/car_rental_api/controller/CarController.java
@@ -0,0 +1,33 @@
+package pl.myproject.car_rental_api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.myproject.car_rental_api.dto.CarDTO;
+import pl.myproject.car_rental_api.service.CarService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService){
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CarDTO>> getCarsWithDetails() {
+
+        List<CarDTO> carDTOS = carService.getAllCarsWithDetails();
+
+        return ResponseEntity.ok(carDTOS);
+    }
+}
